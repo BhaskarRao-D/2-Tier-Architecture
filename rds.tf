8214663a548c997c3d3862e9eeac93ed808b11d6
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-07-09 12:10:08 +0530

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,25 @@
+resource "aws_db_subnet_group" "my_subnet_group" {
+  name       = "my-subnet-group"
+  subnet_ids = [aws_subnet.publicsubnet1.id, aws_subnet.publicsubnet2.id]
+}
+
+resource "aws_db_instance" "database" {
+  allocated_storage         = 10
+  storage_type              = "gp2"
+  identifier                = "database"
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  instance_class            = "db.t2.micro"
+  vpc_security_group_ids    = [aws_security_group.rds-sg.id]
+  db_subnet_group_name      = aws_db_subnet_group.my_subnet_group.name
+  username                  = "bhaskar"
+  password                  = "damuluri98"
+  multi_az                  = true
+  delete_automated_backups  = true
+  parameter_group_name      = "default.mysql5.7"
+  skip_final_snapshot       = true
+  publicly_accessible       = true
+  final_snapshot_identifier = "db-backup"
+  backup_retention_period   = "0"
+  deletion_protection       = false
+}
