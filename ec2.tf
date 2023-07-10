86a27ed79edf8cfaef6778d4e125f15102a6e4b4
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-07-10 12:05:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,18 @@
 resource "aws_instance" "web-server" {
 
-  ami             = "ami-03c7c1f17ee073747"
-  instance_type   = "t2.micro"
-  count           = 2
-  key_name        = "PG"
+  ami                    = "ami-03c7c1f17ee073747"
+  instance_type          = "t2.micro"
+  count                  = 2
+  key_name               = "PG"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id     = element(["subnet-05f83ee6878f655fe", "subnet-0c39950517eeae01b"], count.index)
-  user_data       = <<-EOF
+  subnet_id              = element(["subnet-0948f11e16ac02313", "subnet-000752aac692410af"], count.index)
+  user_data              = <<-EOF
 
-   #!/bin/bash
-   sudo su 
-   yum update -y
-   yum install httpd -y
-   systemctl start httpd
-   systemctl stats httpd
+          #!/bin/bash
+          sudo yum update -y
+          sudo amazon-linux-extras install nginx1 -y 
+          sudo systemctl enable nginx
+          sudo systemctl start nginx
    EOF
 
   tags = {
