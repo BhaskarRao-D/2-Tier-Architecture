741fcac0c8daa5bef8e216a4233a71a3022f0964
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-07-10 12:01:54 +0530

    Update webserver-sg.tf

--- webserver-sg.tf
+++ webserver-sg.tf
@@ -15,7 +15,14 @@ resource "aws_security_group" "webserver-sg" {
     to_port     = "22"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+}
+
+  egress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = "0"
