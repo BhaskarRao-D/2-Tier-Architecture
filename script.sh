43cb9a7ef9eddaf67c1679679077546bce5103b1
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-10-10 10:46:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@ sudo mkfs.ext4 /dev/sdh
 sudo mount /dev/sdh /var/www/html
 sudo rm -rf /var/www/html/*
 sudo yum install git -y
-sudo git clone https://github.com/BhaskarRao-D/Terraform_WebApp-HTML.git  /var/www/html
+sudo git clone https://github.com/BhaskarRao-D/Terraform_HTML.git  /var/www/html
